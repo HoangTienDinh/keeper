1b913dde2920e337eb2450ddfebf4636eca9139e
Hoang Dinh <hoangdinh90@gmail.com>
2020-06-27 00:37:29 -0700

    component Index/Note finished, start of Create

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,16 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  html{
+    font-family: sans-serif;
+  }
+  body{
+    background: #eee;
+    padding: 0 16px;
+  }
 </style>
--- src/components/notes/Index.vue
+++ src/components/notes/Index.vue
@@ -1,32 +1,29 @@
 <template>
-  <ol>
-    <li v-for="note in notes">
-      <pre>
-        {{note | json}}
-      </pre>
-    </li>
-  </ol>
+  <div class="notes">
+    <note v-for="note in notes" :note="note"> </note>
+  </div>
 </template>
 
 <script>
-import Firebase from 'firebase'
+import Firebase from "firebase";
+import Note from "./Note";
 
 export default {
-    data () {
-        return {
-            notes: []
-        }
-    },
-    ready () {
-        let firebase = new Firebase('https://keeper-e7dd6.web.app/')
-        firebase.child('notes').on('child_added', (snapshot) => {
-            let note = snapshot.val()
-            this.notes.unshift(note)
-        })
-    }
-}
+  components: {
+    Note
+  },
+  ready() {
+    let firebase = new Firebase("https://keeper-e7dd6.web.app/");
+    firebase.child("notes").on("child_added", snapshot => {
+      let note = snapshot.val();
+      this.notes.unshift(note);
+    });
+  }
+};
 </script>
 
 <style>
-
+.notes {
+  padding: 0 100px;
+}
 </style>
